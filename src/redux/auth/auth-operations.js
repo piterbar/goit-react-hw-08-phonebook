88d0ba65cae0557b517d1ea30db5f209a3dc757c
Piotr Baraniak <piterb86@gmail.com>
2023-12-07 13:42:07 +0100

    wrong api

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,24 +1,17 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
-const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-  unset() {
-    axios.defaults.headers.common.Authorization = '';
-  },
-};
+// Ustawienie Base URL dla mockAPI
+axios.defaults.baseURL = 'https://6571af27d61ba6fcc01344b4.mockapi.io/';
 
 const register = createAsyncThunk(
   'auth/register',
   async (credentials, thunkAPI) => {
     try {
-      const { data } = await axios.post('/users/signup', credentials);
-      toast.success(`Hi ${data.user.name}, now you are signed up!`);
-      token.set(data.token);
+      // Dostosowanie endpointu do twojego mockAPI
+      const { data } = await axios.post('/contacts', credentials);
+      toast.success(`Hi ${data.name}, now you are signed up!`);
       return data;
     } catch (error) {
       toast.error('Sorry, something went wrong');
@@ -29,47 +22,44 @@ const register = createAsyncThunk(
 
 const logIn = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
   try {
-    const { data } = await axios.post('/users/login', credentials);
-    toast.success(`Hi ${data.user.name}, you are logged in!`);
-    token.set(data.token);
+    // Dostosowanie endpointu do twojego mockAPI
+    const { data } = await axios.post('/contacts', credentials);
+    toast.success(`Hi ${data.name}, you are logged in!`);
     return data;
   } catch (error) {
     toast.error('Something went wrong, check your email or password');
     return thunkAPI.rejectWithValue(error.message);
   }
 });
+
 const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
-    await axios.post('/users/logout');
+    // Uproszczenie operacji wylogowania
     toast.info(`Bye, see you next time!`);
-    token.unset();
   } catch (error) {
     toast.error('Sorry, something went wrong');
     return thunkAPI.rejectWithValue(error.message);
   }
 });
+
 const fetchCurrentUser = createAsyncThunk(
   'auth/refresh',
   async (_, thunkAPI) => {
-    const state = thunkAPI.getState();
-    const persistedToken = state.auth.token;
-
-    if (persistedToken === null) {
-      return thunkAPI.rejectWithValue();
-    }
-    token.set(persistedToken);
     try {
-      const { data } = await axios.get('/users/current');
+      // Dostosowanie endpointu do twojego mockAPI
+      const { data } = await axios.get('/contacts');
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
+
 const operations = {
   register,
   logOut,
   logIn,
   fetchCurrentUser,
 };
+
 export default operations;
--- src/redux/myContacts/contactsOperations.js
+++ src/redux/myContacts/contactsOperations.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://6571af27d61ba6fcc01344b4.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetch',
